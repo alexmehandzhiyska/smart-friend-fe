756c11ac6e08b98dd367eb7b285db8a5629c4e1f
Alexandrina Mehandzhiyska <alexandrina.mehandzhiyska1@gmail.com>
2023-04-01 16:29:40 +0300

    Added written messages option

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,16 +1,31 @@
 import { useState, useRef } from 'react';
 
 import Dictaphone from '../Dictaphone/Dictaphone';
-
 import './Home.css';
+import chatService from '../../services/chatService';
 
 const Home = () => {
     const [reply, setReply] = useState('');
+    const messageRef = useRef(null);
+
+    const sendMessage = () => {
+        const message = messageRef.current.value;
+        
+        chatService.sendText(message)
+            .then((res) => {
+                setReply(message);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }
 
     return (
         <section className="home-page">
             <article id="send-message">
                 <p>{reply}</p>
+                <input ref={messageRef} type="text" name="message" id="message" placeholder="Message" />
+                <button onClick={sendMessage}>Send message</button>
                 <Dictaphone setReply={setReply} />
             </article>
         </section>
