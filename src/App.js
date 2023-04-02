1090da33464d246fedcc50f8d1577bce5837eba8
Alexandrina Mehandzhiyska <alexandrina.mehandzhiyska1@gmail.com>
2023-04-02 03:57:06 +0300

    Design fixes

--- src/App.js
+++ src/App.js
@@ -5,13 +5,16 @@ import Home from './components/Home/Home';
 import Register from './components/Register/Register';
 import Login from './components/Login/Login';
 import Layout from './components/Layout/Layout';
+import { UserRoute } from './components/common/GuardedRoute';
 
 function App() {
   return (
     <BrowserRouter>
       <Layout>
         <Routes>
-          <Route path='/' element={<Home />}></Route>
+          <Route element={<UserRoute />}>
+            <Route path='/' element={<Home />}></Route>
+          </Route>
           <Route path='/register' element={<Register />}></Route>
           <Route path='/login' element={<Login />}></Route>
         </Routes>
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,9 +1,9 @@
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import authService from '../../services/authService';
 import { useForm } from 'react-hook-form';
 import { TextField } from '@mui/material';
 
-import './Login.css'
+import './Login.css';
 
 const Login = () => {
     const { register, handleSubmit, formState: { errors } } = useForm();
@@ -23,11 +23,10 @@ const Login = () => {
     return (
         <section className="content-wrapper">
             <form method="POST" onSubmit={handleSubmit(loginUser)}>
-
-                <h1>Welcome back</h1>
-                <TextField type="username" id="username" name="username" {...register('username')} label="Username" variant="outlined" sx={{my: 2}} color="success" ></TextField>
+                <h1 className="title">Welcome back</h1>
+                <TextField type="username" id="username" name="username" {...register('username')} label="Username" variant="outlined" sx={{my: 2}} color="success"></TextField>
                 <TextField type="password" id="password" name="password" {...register('password')} label='Password' variant="outlined" sx={{my: 2}} color="success"></TextField>
-
+                <p>Don't have an account? <Link to="/register">Register here</Link></p>
                 <input type="submit" value="Submit" className="submit primary-btn"/>
             </form>
         </section>
